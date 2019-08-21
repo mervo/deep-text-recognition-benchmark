fdaaba867daadc33631c2f726b3d887b797e0045
user <9879403+mervo@users.noreply.github.com>
2019-08-21 17:47:15 +0800

    First working environment.

---
+++ run.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python3 demo.py --Transformation TPS --FeatureExtraction ResNet --SequenceModeling BiLSTM --Prediction Attn --image_folder demo_image/ --saved_model TPS-ResNet-BiLSTM-Attn.pth
